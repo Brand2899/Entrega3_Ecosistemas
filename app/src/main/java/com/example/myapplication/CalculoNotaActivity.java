50cb9c247e7a90ca82ba7dd16dec74cbe5cd5142
Brand28 <alejandrobrand28@gmail.com>
2021-09-06 04:29:11 -0500

    pantalla notas fallo, no deja avanzar

--- app/src/main/java/com/example/myapplication/CalculoNotaActivity.java
+++ app/src/main/java/com/example/myapplication/CalculoNotaActivity.java
@@ -2,13 +2,52 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculoNotaActivity extends AppCompatActivity {
+
+    private EditText et1, et2, et3, et4, et5;
+    private TextView tv1;
+    private Button bn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo_nota);
+
+        et1= findViewById(R.id.et1);
+        et2= findViewById(R.id.et2);
+        et3= findViewById(R.id.et3);
+        et4= findViewById(R.id.et4);
+        et5= findViewById(R.id.et5);
+
+        tv1 = findViewById(R.id.tv1);
+
+        bn1 = findViewById(R.id.bn1);
+
+
+        bn1.setOnClickListener(
+                (v)->{
+
+                    if(!et1.getText().toString().isEmpty() && et2.getText().toString().isEmpty() && et3.getText().toString().isEmpty() && et4.getText().toString().isEmpty() && et5.getText().toString().isEmpty()) {
+
+                        Intent i = new Intent(this, MainActivity.class);
+                        startActivity(i);
+
+                    }else{
+                        Toast.makeText(this,"Colocar todas las notas", Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+        );
+
+
+
     }
 }
